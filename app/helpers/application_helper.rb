e7140f5a2f104c061014dd541bc3285112811bda
Roman <gnemot@gmail.com>
2011-10-24 18:18:12 +0500

    Changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
 
 
   def link_to_cart(text = t('cart'))
-    return "" if current_page?(cart_path)
+    #return "" if current_page?(cart_path)
     css_class = nil
     if current_order.nil? or current_order.line_items.empty?
       text = "#{text}"
