c055d80d6f973f33ddd9abf06f67f5a1decf060f
Roman <gnemot@gmail.com>
2011-12-12 22:17:19 +0500

    Many changes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Project::Application.config.session_store :cookie_store, :key => '_project_session'
+Project::Application.config.session_store :cookie_store, :key => '_project_session', :expires => 15.hours.from_now
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
