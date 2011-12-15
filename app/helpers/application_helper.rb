a4d004554b68183c09e277e690e718d20e531bdc
Roman <gnemot@gmail.com>
2011-12-15 11:37:56 +0500

    Changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,11 +22,15 @@ module ApplicationHelper
 
   def timetable
     timetable = "";
+    params["month"] = Date.today.month unless params["month"]
+    params["year"] = Date.today.year unless params["year"]
     require 'net/http'
     h = Net::HTTP.new("www.artbanda.com", 80)
     h.read_timeout = 500
-    resp = h.get("/school/timetable/",nil)
+    resp = h.get("/school/timetable/?month=#{params["month"]}&year=#{params["year"]}",nil)
     timetable = Iconv.iconv('utf-8', 'windows-1251',  resp.read_body).to_s.match(/(<table)([^>]*)(class="cld_month">)(.*?)(<\/table>)/)
-    "<table class='timetable'>#{timetable[4].html_safe.gsub(/h1/,'b').gsub(/font:14px arial;/, 'font:14px arial; text-align: center;').gsub(/style="font: 12px arial;"/,'').gsub(/span><span class="title"/, 'span><br/><span class="cld_title"')}</table>".html_safe
+    pre_link = Iconv.iconv('utf-8', 'windows-1251',  resp.read_body).to_s.match(/(<a class="prev" href=")([^">]*)(">&larr; )(.*?)(<\/a>)/)
+    next_link = Iconv.iconv('utf-8', 'windows-1251',  resp.read_body).to_s.match(/(<a class="next" href=")([^">]*)(">)(.*?)( &rarr;<\/a>)/)
+    "<table class='timetable'>#{timetable[4].html_safe.gsub(/h1/,'b').gsub(/font:14px arial;/, 'font:14px arial; text-align: center;').gsub(/style="font: 12px arial;"/,'').gsub(/span><span class="title"/, 'span><br/><span class="cld_title"')}</table><a class='pre_school_link' href='/school/timetable#{pre_link[2]}'>&larr; #{pre_link[4]}</a><a class='next_school_link' href='/school/timetable#{next_link[2]}'>&rarr; #{next_link[4]}</a>".html_safe
   end
 end
