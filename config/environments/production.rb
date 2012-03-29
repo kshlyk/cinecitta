237843a295a6445f24c1a7fb158ed189c5286b56
Roman <gnemot@gmail.com>
2012-03-29 15:51:18 +0500

    Changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Project::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  Paperclip.options[:command_path] = "/usr/local/bin"
+  
 end
