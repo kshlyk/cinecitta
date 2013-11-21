25dfdc115bd55a42dd50c25d2f41e311807a390f
Babur Usenakunov <bob.usenakunov@gmail.com>
2013-11-21 21:48:09 +0600

    Gallery changed

--- app/models/gallery_item_decorator.rb
+++ app/models/gallery_item_decorator.rb
@@ -1,6 +1,6 @@
 GalleryItem.class_eval do
   has_attached_file :attachment, 
-                    :styles => { :mini => '100x100#', :cropper => '600', :normal => '300x300#', :large => '800x800>' },
+                    :styles => { :mini => '100x100#', :cropper => '600', :normal => '300x300#', :large => '1300x800>' },
                     :default_style => :normal,
                     :url => "/assets/gallery_items/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/gallery_items/:id/:style/:basename.:extension"
